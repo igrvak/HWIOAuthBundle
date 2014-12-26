da84fe305760466b22caa40a0e3f21459dc3625a
Vladimir Odesskij <odesskij1992@gmail.com>
2014-12-26 20:06:52 +0200

    social

--- src/Iwin/Bundle/AppBundle/Resources/scripts/jsmodel/social/facebook.js
+++ src/Iwin/Bundle/AppBundle/Resources/scripts/jsmodel/social/facebook.js
@@ -1,11 +1,52 @@
 define([
-    'facebook-api'
-], function (FB) {
+    'facebook-api',
+    'backbone'
+], function (FB, Backbone) {
     'use strict';
 
     FB.init({
         appId: '774836025885842', //TODO: bidlokod
     });
 
-    return FB;
+
+    var Model = Backbone.Model.extend({
+        isLogged: function (ifTrue, ifFalse) {
+            FB.getLoginStatus(function (response) {
+                if (response.status === 'connected') {
+                    ifTrue();
+                }
+                else {
+                    ifFalse();
+                }
+            });
+        },
+        login:    function (ifSuccess, ifFail) {
+            FB.login(function (response) {
+                if (response.status === 'connected') {
+                    ifSuccess();
+                }
+                else {
+                    ifFail();
+                }
+            }, {
+                scope: 'user_about_me'
+            });
+
+        },
+        getData:  function (callback) {
+            FB.api('/me', function (response) {
+                callback({
+                    id:     response.id,
+                    name:   response.name,
+                    gender: response.gender,
+                    link:   response.link,
+                    photo:  ''
+                });
+            });
+        }
+    });
+
+    var Facebook = new Model({});
+
+    return Facebook;
 });
--- src/Iwin/Bundle/AppBundle/Resources/views/Home/social.js
+++ src/Iwin/Bundle/AppBundle/Resources/views/Home/social.js
@@ -2,132 +2,156 @@ requirejs([
     'jquery',
     'underscore',
     'backbone',
-    'hello'
-], function ($, _, Backbone, hello) {
+    'social/facebook',
+    'social/linkedin'
+], function ($, _, Backbone, facebook, linkedin) {
     'use strict';
-
-    console.log(hello);
-
-    hello.init({
-        twitter:  'FbZDGmk9kvfwY3yxsLopIfOfE',
-        google:   '162631910090-igsi864ajqpp1k3k4kl5lfftcrdfsct0.apps.googleusercontent.com',
-        linkedin: '75l9jttrjuc4a2',
-        facebook: '774836025885842'
-    }, {redirect_uri: '/'});
+    //
+    console.log(linkedin);
 
 
-    $('#twitter').on('click', function (event) {
+    $('#facebook').on('click', function (event) {
         event.preventDefault();
-        hello.login('twitter', {display: 'none'});
-    });
+        facebook.login(function () {
+            facebook.getData(function (a) {
+                $('#facebook').text('IS READY, ' + a.name + '!');
+            });
 
-    $('#g').on('click', function (event) {
-        event.preventDefault();
-        hello.login('google', {display: 'none'});
+        });
     });
 
     $('#linkedin').on('click', function (event) {
         event.preventDefault();
-        hello.login('linkedin', {display: 'none'});
+        linkedin.login(function () {
+            linkedin.getData(function (a) {
+                $('#linkedin').text('IS READY, ' + a.name + '!');
+            });
+        });
     });
 
-    $('#facebook').on('click', function (event) {
-        event.preventDefault();
-        hello.login('facebook', {display: 'none'});
-    });
-
-    //hello.on('auth.login', function (r) {
-    //    // Get Profile
-    //    hello.api(r.network + ':/me', function (p) {
-    //        console.log(p);
-    //    });
-    //});
     //
+    //console.log(hello);
     //
     //hello.init({
-    //    twitter:  '9xPg0TbmOIU3uLpUs6piWGhCF',
-    //    facebook: '774836025885842',
-    //    linkedin: '75l9jttrjuc4a2'
-    //}, {
-    //    redirect_uri: '/',
+    //    twitter:  'FbZDGmk9kvfwY3yxsLopIfOfE',
+    //    google:   '162631910090-igsi864ajqpp1k3k4kl5lfftcrdfsct0.apps.googleusercontent.com',
+    //    linkedin: '75l9jttrjuc4a2',
+    //    facebook: '774836025885842'
+    //}, {redirect_uri: '/'});
+    //
+    //
+    //$('#twitter').on('click', function (event) {
+    //    event.preventDefault();
+    //    hello.login('twitter');
     //});
     //
-    //hello.login('twitter');
-
-    //
-    //(function login(network) {
-    //    // Twitter instance
-    //    var twitter = hello(network);
-    //    // Login
-    //    twitter.login().then(function (r) {
-    //        // Get Profile
-    //        return twitter.api('me');
-    //    })
-    //        .then(function (p) {
-    //            // Put in page
-    //            console.log(p);
-    //        });
-    //})('twitter');
-    //
-    //hello.init({
-    //        'twitter': '9xPg0TbmOIU3uLpUs6piWGhCF'
-    //    },
-    //    {
-    //        redirect_uri: document.URL
-    //    });
-
-
-    //setTimeout(function () {
-    //    gapi.client.plus.people.get({
-    //        'userId': 'me'
-    //    }).then(function (res) {
-    //        var profile = res.result;
-    //        console.log(profile);
-    //    });
-    //}, 3000);
-
-    //
-    //console.log(IN);
-
-    //IN.API.Profile("me").result(function (data) {
-    //    console.log(data);
+    //$('#g').on('click', function (event) {
+    //    event.preventDefault();
+    //    hello.login('google');
     //});
     //
-    //
-    //
-    //
-    //FB.getLoginStatus(function (response) {
-    //    if (response.status === 'connected') {
-    //        FB.api(
-    //            "/me",
-    //            {
-    //                access_token: response.authResponse.accessToken
-    //            },
-    //            function (response) {
-    //                if (response && !response.error) {
-    //                    console.log('facebook', response);
-    //                }
-    //            }
-    //        );
-    //        console.log('Logged in.');
-    //    }
-    //    else {
-    //        FB.login(function () {
-    //        }, {
-    //            scope: 'user_about_me'
-    //        });
-    //    }
-    //});
-
-
-    //
-    //var Model = Backbone.Model.extend({
-    //    'class':   'twitter',
-    //    'getData': function () {
-    //        return 'some-data';
-    //    }
+    //$('#linkedin').on('click', function (event) {
+    //    event.preventDefault();
+    //    hello.login('linkedin');
     //});
     //
-    //var twitter = new Model();
+    //$('#facebook').on('click', function (event) {
+    //    event.preventDefault();
+    //    hello.login('facebook');
+//});
+
+//hello.on('auth.login', function (r) {
+//    // Get Profile
+//    hello.api(r.network + ':/me', function (p) {
+//        console.log(p);
+//    });
+//});
+//
+//
+//hello.init({
+//    twitter:  '9xPg0TbmOIU3uLpUs6piWGhCF',
+//    facebook: '774836025885842',
+//    linkedin: '75l9jttrjuc4a2'
+//}, {
+//    redirect_uri: '/',
+//});
+//
+//hello.login('twitter');
+
+//
+//(function login(network) {
+//    // Twitter instance
+//    var twitter = hello(network);
+//    // Login
+//    twitter.login().then(function (r) {
+//        // Get Profile
+//        return twitter.api('me');
+//    })
+//        .then(function (p) {
+//            // Put in page
+//            console.log(p);
+//        });
+//})('twitter');
+//
+//hello.init({
+//        'twitter': '9xPg0TbmOIU3uLpUs6piWGhCF'
+//    },
+//    {
+//        redirect_uri: document.URL
+//    });
+
+
+//setTimeout(function () {
+//    gapi.client.plus.people.get({
+//        'userId': 'me'
+//    }).then(function (res) {
+//        var profile = res.result;
+//        console.log(profile);
+//    });
+//}, 3000);
+
+//
+//console.log(IN);
+
+//IN.API.Profile("me").result(function (data) {
+//    console.log(data);
+//});
+//
+//
+//
+//
+//FB.getLoginStatus(function (response) {
+//    if (response.status === 'connected') {
+//        FB.api(
+//            "/me",
+//            {
+//                access_token: response.authResponse.accessToken
+//            },
+//            function (response) {
+//                if (response && !response.error) {
+//                    console.log('facebook', response);
+//                }
+//            }
+//        );
+//        console.log('Logged in.');
+//    }
+//    else {
+//        FB.login(function () {
+//        }, {
+//            scope: 'user_about_me'
+//        });
+//    }
+//});
+
+
+//
+//var Model = Backbone.Model.extend({
+//    'class':   'twitter',
+//    'getData': function () {
+//        return 'some-data';
+//    }
+//});
+//
+//var twitter = new Model();
 
 });

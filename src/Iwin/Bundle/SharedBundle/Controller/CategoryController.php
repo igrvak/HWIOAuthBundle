5d0cd8b484b87f12eb176be878a40556c5321000
Bogdan Yurov <bogdan@yurov.me>
2014-12-28 15:21:30 +0200

    google #11675

--- src/Iwin/Bundle/SharedBundle/Controller/CategoryController.php
+++ src/Iwin/Bundle/SharedBundle/Controller/CategoryController.php
@@ -3,6 +3,8 @@ namespace Iwin\Bundle\SharedBundle\Controller;
 
 use Entity\Category;
 use Entity\Repository\CategoryRepository;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use JMS\DiExtraBundle\Annotation as DI;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**

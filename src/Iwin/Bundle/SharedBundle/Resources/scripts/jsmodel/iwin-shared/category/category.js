fe8a19093e95114c2b573000a98bb44dd1b15dc7
Bogdan Yurov <bogdan@yurov.me>
2014-12-28 14:51:49 +0200

    category подготовка

---
+++ src/Iwin/Bundle/SharedBundle/Resources/scripts/jsmodel/iwin-shared/category/category.js
@@ -0,0 +1,12 @@
+define([
+    'lodash',
+    'backbone',
+    'util/basemodel',
+], function (_, Backbone, BaseModel) {
+    'use strict';
+
+    var Model = BaseModel.extend({
+    });
+
+    return Model;
+});

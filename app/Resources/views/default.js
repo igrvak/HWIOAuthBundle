9ae306525ef787c7842efe952b53f5f987f9d89b
vakula <igrvak@gmail.com>
2015-01-05 18:12:36 +0200

    fix

--- app/Resources/views/default.js
+++ app/Resources/views/default.js
@@ -4,8 +4,28 @@ requirejs([
 ], function ($) {
     'use strict';
 
-    $('a[href="#user_profile"], a[href="#need_help"], a[href="#send_invite"]').click(function (e) {
+    $('a[href="#need_help"], a[href="#send_invite"]').click(function (e) {
         e.preventDefault();
         window.alert('Todo: не готово');
     });
+
+    //sign in pop up
+    (function() {
+        var body = $('body'),
+            loginPopup = $('#login-popup'),
+            popupClose = loginPopup.find('.close-popup'),
+            loginLink = $('#header').find('a.account');
+
+        function popupToggle(link) {
+            link.on('click', function(e){
+                e.preventDefault(e);
+                loginPopup.toggleClass('popup-active');
+                if (loginPopup.hasClass('popup-active')) {
+                    body.addClass('lock');
+                } else {body.removeClass('lock');}
+            })
+        }
+        popupToggle(popupClose);
+        popupToggle(loginLink);
+    })()
 });
